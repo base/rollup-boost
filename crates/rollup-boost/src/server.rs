77b05e8ea8c216f96116e3106aa696c9e5b9c684
Cody Wang <cody.wang@coinbase.com>
2025-11-18 02:47:56 -0300

    not mark unhealthy if missing payload

--- crates/rollup-boost/src/server.rs
+++ crates/rollup-boost/src/server.rs
@@ -198,7 +198,7 @@ where
                 tracing::Span::current().record("builder_has_payload", false);
                 return BuilderResult::Ok(BuilderPayloadResult {
                     payload: None,
-                    builder_api_failed: true,
+                    builder_api_failed: false,
                 });
             }
 
@@ -536,7 +536,7 @@ where
             // forward the fcu to the builder to keep it synced and immediately return the l2
             // response without awaiting the builder
             let builder_client = self.builder_client.clone();
-            let attrs_clone = payload_attributes.clone();
+            let attrs_clone: Option<OpPayloadAttributes> = payload_attributes.clone();
             tokio::spawn(async move {
                 // It is not critical to wait for the builder response here
                 // During moments of high load, Op-node can send hundreds of FCU requests
